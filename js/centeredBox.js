4da98af61736d87f478754adf59a796846d01753
picpork <picpork@2548d6ba-e976-3bc9-2b10-e45cd972f336>
2011-01-15 14:13:21 +0000

    Added new Dimension and added center javascript for IE

---
+++ js/centeredBox.js
@@ -0,0 +1,7 @@
+function setPosition(){
+	var centeredBox=$('#speakker');
+	var w=$(document).width(), tw= centeredBox.width();
+	centeredBox.css("left", (w-tw>0?((w/2)-(tw/2))+'px':0) );
+};
+	window.onload=function(){setPosition()};
+	window.onresize=function(){setPosition()};
