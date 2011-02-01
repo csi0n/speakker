d8f5a4a8cf7bd6a82f17b3a260459751cb963677
picpork <picpork@2548d6ba-e976-3bc9-2b10-e45cd972f336>
2011-02-01 05:01:12 +0000

    Modified player to class and added a light variant

--- js/centeredBox.js
+++ js/centeredBox.js
@@ -1,5 +1,5 @@
 function setPosition(){
-	var centeredBox=$('#speakker');
+	var centeredBox=$('.speakker');
 	var w=$(document).width(), tw= centeredBox.width();
 	centeredBox.css("left", (w-tw>0?((w/2)-(tw/2))+'px':0) );
 };
